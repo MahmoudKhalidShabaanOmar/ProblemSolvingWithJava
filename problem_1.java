dcf772760f9824e7edf2eeb428653f02d170c130
MahmoudKhalidShabaanOmar <m.k.s.omar1112003@gmail.com>
2024-12-20 15:37:09 +0300

    Delete problem_1.java

--- problem_1.java
+++
@@ -1,6 +0,0 @@
-// write a java program to print welcome , and hello message for the user =>
-public class welcome{
-    public static void main(String[] args){
-        System.out.print("Hello EveryOne! From Different Countery , And Welcome To Frist Java Program") ;
-    }
-}
