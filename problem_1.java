7a59e98a9ee9f512f30e5e5f74784c973dd90c4e
MahmoudKhalidShabaanOmar <m.k.s.omar1112003@gmail.com>
2024-12-20 14:08:01 +0300

    Add files via upload

---
+++ problem_1.java
@@ -0,0 +1,6 @@
+// write a java program to print welcome , and hello message for the user =>
+public class welcome{
+    public static void main(String[] args){
+        System.out.print("Hello EveryOne! From Different Countery , And Welcome To Frist Java Program") ;
+    }
+}
